69f37e061ec636b114de75a6d9f8b62651fc72a5
Red-Skull <solorzano.chelo@gmail.com>
2016-11-17 10:01:21 -0600

    Sugerencias para la barra de busqueda.

--- app/src/main/java/com/eniac/eniacs/realidadaumentadaucr/MapsActivity.java
+++ app/src/main/java/com/eniac/eniacs/realidadaumentadaucr/MapsActivity.java
@@ -30,7 +30,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
